d44237b72025b6414b8b3e1b9702a7f15316dbdb
bryce-sampson <sampsonbrycea@gmail.com>
2019-06-29 18:56:23 -0400

    added links to get back to main pages, added some css

--- src/main/java/WCCI/proofofconcept/Initializer.java
+++ src/main/java/WCCI/proofofconcept/Initializer.java
@@ -29,21 +29,33 @@ public class Initializer implements CommandLineRunner {
 		Genre alternative = new Genre("Alternative Rock");
 		Genre classical = new Genre("Classical");
 		Genre rock = new Genre("Rock");
+		Genre country = new Genre("Country");
+		Genre rap = new Genre("Rap");
+		Genre pop = new Genre("Pop");
+		Genre edm = new Genre("EDM");
+		Genre blues = new Genre("Blues");
 		genreRepo.save(alternative);
 		genreRepo.save(classical);
 		genreRepo.save(rock);
+		genreRepo.save(country);
+		genreRepo.save(rap);
+		genreRepo.save(pop);
+		genreRepo.save(edm);
+		genreRepo.save(blues);
 		PostTag keyboard = new PostTag("Keyboard");
 		PostTag piano = new PostTag("Piano");
+		PostTag orchestra = new PostTag("Orchestra");
 		PostTag cowboyWestern = new PostTag("Cowboy Western");
 		PostTag oldWestRock = new PostTag("Old West Rock");
 		postTagRepo.save(keyboard);
 		postTagRepo.save(piano);
+		postTagRepo.save(orchestra);
 		postTagRepo.save(cowboyWestern);
 		postTagRepo.save(oldWestRock);
 		postRepo.save(new Post("Cough Syrup", authorRepo.findByName("Young the Giant"), alternative, "2008", "Mellow guitar and cello contrast the sunny preceding tracks, the lyrics contemplate apathy and misdirection, and yet the chorus is still so compelling that you find yourself humming it hours later."));
-		postRepo.save(new Post("Symphony No. 5", authorRepo.findByName("Ludwig van Beethoven"), alternative, "1808", "Radiant beams shoot through this region's deep night, and we become aware of gigantic shadows which, rocking back and forth, close in on us and destroy everything within us except the pain of endless longing—a longing in which every pleasure that rose up in jubilant tones sinks and succumbs, and only through this pain, which, while consuming but not destroying love, hope, and joy, tries to burst our breasts with full-voiced harmonies of all the passions, we live on and are captivated beholders of the spirits."));
+		postRepo.save(new Post("Symphony No. 5", authorRepo.findByName("Ludwig van Beethoven"), classical, "1808", "Radiant beams shoot through this region's deep night, and we become aware of gigantic shadows which, rocking back and forth, close in on us and destroy everything within us except the pain of endless longing—a longing in which every pleasure that rose up in jubilant tones sinks and succumbs, and only through this pain, which, while consuming but not destroying love, hope, and joy, tries to burst our breasts with full-voiced harmonies of all the passions, we live on and are captivated beholders of the spirits."));
 		postRepo.save(new Post("Outlaw Gentlemen", authorRepo.findByName("Volbeat"), rock, "2016", "Why are these danish lads such fans of the old american western frontier"));
-		postRepo.save(new Post("Guitar Gangsters", authorRepo.findByName("Volbeat"), rock, "2008", "this music was not in a saloon"));
+		postRepo.save(new Post("Guitar Gangsters", authorRepo.findByName("Volbeat"), rock, "2008", "This music was not in a saloon"));
 		
 	}
 
